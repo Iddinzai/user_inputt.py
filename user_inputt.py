76f42b7abc0f85c4e3ff0e34849739a229083602
Iddinzai <eididdyz@gmail.com>
2024-10-02 12:00:12 +0300

    first commit

---
+++ user_inputt.py
@@ -0,0 +1,6 @@
+name=input("what is your name? ")
+age= input("what is your age? ")
+location=input("Where do you stay? ")
+print(f"hello {name} you are {age} years old and you live at {location}")
+
+
